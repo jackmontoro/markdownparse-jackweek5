01cee3ff4186e4a31afb1167d3ebc425b1a65d5c
ShashankVenkatramani <cjvshashank@gmail.com>
2022-01-19 16:30:36 -0800

    Update MarkdownParse.java
    Added fix for if there is only a parenthesis at the end.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -14,14 +14,23 @@ public class MarkdownParse {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
+            
+            if(nextOpenBracket == -1) {
+                break;
+            }
+            
             if(openParen == -1) {
                 openParen = nextCloseBracket;
+            }
+            int closeParen = markdown.indexOf(")", openParen);
+            if(closeParen > markdown.indexOf("\n", openParen)) {
+                closeParen = -1;
             }
             if(closeParen == -1) {
                 closeParen = markdown.indexOf("\n", openParen);
             }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            // System.out.println("NOB: " + nextOpenBracket + " NCB: " + nextCloseBracket + " OP: " + openParen + " CP: " + closeParen);
             currentIndex = closeParen + 1;
         }
         return toReturn;
@@ -29,6 +38,7 @@ public class MarkdownParse {
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
+        // System.out.println(contents);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
