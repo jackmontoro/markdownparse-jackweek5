8418c4e0c488ae30fb71ef42098339c536f5dfe5
ShashankVenkatramani <cjvshashank@gmail.com>
2022-01-26 14:38:29 -0800

    Not working stage

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,15 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(2, 1 + 1);
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
     }
 }
