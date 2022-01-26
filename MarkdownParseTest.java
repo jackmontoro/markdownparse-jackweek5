1d3456cc805c7e6c82ba62c7934d95bb49169e3d
ShashankVenkatramani <cjvshashank@gmail.com>
2022-01-26 14:43:49 -0800

    All four tests run

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,9 +7,27 @@ import java.util.List;
 
 public class MarkdownParseTest {
     @Test
-    public void addition() throws IOException {
+    public void testDefaultLinkMethod() throws IOException {
         Path fileName = Path.of("test-file.md");
         String contents = Files.readString(fileName);
         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void testOneLinkMethod() throws IOException {
+        Path fileName = Path.of("markdown-new-test.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("thiswillbreak.com", "thisisover.com", "peasy.html"), MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void testTwoLinkMethod() throws IOException {
+        Path fileName = Path.of("markdown-test-two.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("thiswillbreak.com", "thisisover.com", "peasy.html"), MarkdownParse.getLinks(contents));
+    }
+    @Test
+    public void testThreeLinkMethod() throws IOException {
+        Path fileName = Path.of("markdown-test-three.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("thiswillbreak.com", "thisisover.com", "peasy.html"), MarkdownParse.getLinks(contents));
     }
 }
